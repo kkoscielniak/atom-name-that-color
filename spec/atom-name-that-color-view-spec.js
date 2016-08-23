f49dda996df367d6169b61adf6f81406a61a06bd
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2016-08-23 23:35:24 +0200

    initial commit

---
+++ spec/atom-name-that-color-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomNameThatColorView from '../lib/atom-name-that-color-view';
+
+describe('AtomNameThatColorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
